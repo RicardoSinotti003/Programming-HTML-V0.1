7d4546e4f63b93ac53def2631d7c155be1d9d41b
RicardoSinotti003 <ricardosinotti003@gmail.com>
2024-07-28 22:12:28 -0300

    Edit

--- forms.js
+++ forms.js
@@ -1,7 +1,19 @@
 const forms = window.document.getElementById('formIntegration');
 
+function validateName(validateFullName){
+    const nameFull = validateFullName.split(' ');
+    return nameFull.length >= 2;
+}
+
 forms.addEventListener('submit', function(e){
     e.preventDefault();
+
+    const Name = window.document.getElementById('Name');
+    if (!validateName(Name.value)){
+        alert('Please fill in your full name');
+    } else {
+        alert("All right")
+    }
 })
 
 console.log(forms);
