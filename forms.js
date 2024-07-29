a5de63b37dcb49199f69860207194f06ad617ec6
RicardoSinotti003 <ricardosinotti003@gmail.com>
2024-07-29 01:39:15 -0300

    Edit

--- forms.js
+++ forms.js
@@ -8,11 +8,21 @@ function validateName(validateFullName){
 forms.addEventListener('submit', function(e){
     e.preventDefault();
 
+    const messageSuccess = '<b>Form sent successfully<b />';
+    const messageFail = '<b>Incomplete Name<b />';
+    const yourName = window.document.getElementById('Name');
     const Name = window.document.getElementById('Name');
     if (!validateName(Name.value)){
         alert('Please fill in your full name');
+        const msgFail = document.querySelector('.fail-message');
+        msgFail.innerHTML = messageFail;
+        msgFail.style.display = 'block';
+        yourName.style.background = 'red';
     } else {
-        alert("All right")
+        alert("All right");
+        const msgSuccess = document.querySelector('.success-message');
+        msgSuccess.innerHTML = messageSuccess;
+        msgSuccess.style.display = 'block';
     }
 })
 
